7c49db37804680951efef472502a81541cd15e29
Michael Jancik-Kitowski <michael.jancik24@gmail.com>
2023-12-17 17:07:50 -0600

    Made most of the tests work will work on it more over the week

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,107 @@
+// const { get } = require("../../routes/todos.router");
+
 console.log('JS is sourced!');
+
+let todosTBody = document.getElementById('viewTodos');
+
+function getTodos(){
+    axios({
+        method: "GET",
+        url: "/todos"
+    })
+    .then((response) => {
+        placeTodosOnTheDom(response.data);
+        console.log(response.data);
+    })
+    .catch((error) => {
+        console.log("Woops somthing went wrong in the GET");
+        console.log(error);
+    })
+}
+
+function addTodos(event) {
+    event.preventDefault()
+    let todo = {};
+    todo.text = document.getElementById('addTodo').value;
+    console.log("Todos: ", todo);
+    // todo make sure to add in the POST function here once its done
+    sendTodos(todo)
+}
+
+function clearInputs(){
+    document.getElementById('addTodo') = '';
+}
+
+function sendTodos(todoAdded){
+    console.log("Making sure that the inputs are added for todoAdded: ", todoAdded);
+
+    axios ({
+        method: "POST",
+        url: "/todos",
+        data: todoAdded
+    })
+    .then(function (response){
+        console.log("inside sendTodos()", response.data);
+        getTodos();
+        clearInputs();
+    })
+    .catch(function (error) {
+        console.log("Error in POST:", error);
+        alert("Unable to add a todo at this time. Please try again later!")
+    })
+}
+
+function todoMarkedForComplete(event) {
+
+    console.log('incoming event.target', event.target)
+    console.log('Getting dataset from component', event.target.closest("tr").dataset.id)
+  
+    // Retrieving data that has been stored on an element
+    let todoId = event.target.closest("tr").dataset.id
+  
+    axios.put(`/todos/${todoId}`)
+        .then((response) => {
+            getTodos();
+        })
+        .catch((error) => {
+            console.log('Error', error);
+            alert('Something went wrong');
+        });
+  }
+  
+  function deleteTodo(event) {
+    const id = event.target.closest("tr").dataset.id;
+    console.log("id of row to delete:", id);
+  
+    axios({
+      method: "DELETE",
+      url: `/todos/${id}`
+    })
+    .then((response) => {
+      console.log("response:", response.data);
+      // refresh the table
+      getTodos();
+    })
+    .catch((error) => {
+      console.log("whoops, there be an error in here!");
+      console.error(error);
+    })
+  }
+
+
+function placeTodosOnTheDom(arrayOfTodos){
+    todosTBody.innerHTML = "";
+    console.log(todosTBody);
+    for(let todo of arrayOfTodos){
+        todosTBody.innerHTML += `
+            <tr data-testid="toDoItem" data-id="${todo.id}">
+            <td>${todo.text}</td>
+            <td>${todo.isComplete}</td>
+            <td><button data-testid="completeButton" onclick="todoMarkedForComplete(event)">Mark as Complete</button></td>
+            <td><button data-testid="deleteButton" onclick="deleteTodo(event)">Delete</button></td>
+            </tr>
+        `;
+    }
+}
+
+getTodos()
--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -8,7 +8,7 @@ router.get("/", (req, res) => {
     
     pool.query(queryText)
     .then((result) => {
-      // console.log(result.rows);
+      console.log(result.rows);
       console.log("TODOS Backend is running");
       res.send(result.rows);
     })
@@ -45,7 +45,44 @@ router.post("/", (req, res) => {
         })
 })
 
+router.put("/:id", (req, res) => {
+    let todoId = req.params.id;
+    console.log("todoID", todoIdId);
+    let queryText;
+    let queryParams = [todoId];
+    pool
+      .query(queryText, queryParams)
+      .then((result) => {
+        res.sendStatus(204);
+      })
+      .catch((error) => {
+        console.log("Woops, error making query: ", queryText);
+        console.error(error);
+        res.sendStatus(500);
+      });
+  });
 
+  router.delete("/:id", (req, res) => {
+    // Accessing the ID directly from req.params
+    // No need to assign it to another variable like reqId
+    let todoId = req.params.id;
+    console.log("todo id:", todoId);
+  
+    // SQL query to delete the book with the specified ID
+    let sqlText = "DELETE FROM todos WHERE id=$1;";
+  
+    // Executing the query using the pool object
+    pool
+      .query(sqlText, [todoId])
+      .then((result) => {
+        console.log("todo ElImInAtEd");
+        res.sendStatus(200); // Send success status
+      })
+      .catch((error) => {
+        console.log(`Error making database query ${sqlText}`, error);
+        res.sendStatus(500); // Send error status if there's a problem
+      });
+  });
 
 
 module.exports = router;
